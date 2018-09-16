d6975e438a7f93a95c00b8280ba4c65122a71838
Larry Shaffer <lshaffer@boundlessgeo.com>
2018-09-16 17:55:45 -0600

    Migrate to setting just missing AnacondaRecipes env vars

--- recipe/deactivate.sh
+++ recipe/deactivate.sh
@@ -2,25 +2,38 @@
 
 if [ "$(uname)" == "Darwin" ];
 then
-    # for Mac OSX
-    unset CC
-    unset CXX
-    unset MACOSX_VERSION_MIN
-    unset MACOSX_DEPLOYMENT_TARGET
-    unset CMAKE_OSX_DEPLOYMENT_TARGET
-    unset CFLAGS
-    unset CXXFLAGS
-    unset LDFLAGS
-    unset LINKFLAGS
-elif [ "$(uname)" == "Linux" ]
-then
-    # for Linux
-    unset CC
-    unset CXX
-    unset CFLAGS
-    unset CXXFLAGS
-    unset LDFLAGS
-    unset LINKFLAGS
+    # Only for Mac OSX
+    # Remove env vars not in conda-forge toolchain
+    unset AR
+    unset AS
+    
+    unset CLANG
+    unset CLANGXX
+    unset CODESIGN_ALLOCATE
+    unset HOST
+    unset INSTALL_NAME_TOOL
+    unset LD
+    unset LIBTOOL
+    unset LIPO
+    unset NM
+    unset NMEDIT
+    unset OTOOL
+    unset PAGESTUFF
+    unset RANLIB
+    unset REDO_PREBINDING
+    unset SEGEDIT
+    unset SIZE
+    unset STRINGS
+    unset STRIP
+    
+    # Remove env vars, which are part of cctools, though not needed
+    unset CHECKSYMS
+    unset INDR
+    unset SEG_ADDR_TABLE
+    unset SEG_HACK
+    
+    # A fixup for conda-forge
+    unset CPPFLAGS
 else
     echo "This system is unsupported by our toolchain."
     exit 1
